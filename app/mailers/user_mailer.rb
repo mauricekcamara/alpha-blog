816cda9e45d6927522eb301be8e9ff4565e254fc
Maurice Camara <mauricekcamara@gmail.com>
2016-10-17 02:06:01 +0000

    updated contact form email info

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,11 @@
 class UserMailer < ApplicationMailer
   default from: "alzebo2000@gmail.com"
-  
+
   def contact_form(email, name, message)
+    @name = name
     @message = message
+    @email = email
+
       mail(:from => email,
            :to => 'mauricekcamara@gmail.com',
            :subject => "a new contact form message from #{name}")
